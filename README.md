144dcedc398b3c6dd60484c26c83d68767e71118
Yavor Fingarov <yavor.fingarov@outlook.com>
2024-08-15 08:55:29 +0300

    Updated validation logic (#5)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SimpleNewTab
 
-[![api status](https://img.shields.io/uptimerobot/status/m797417947-fe687be74c0746276cc9cb3d?label=api_status)](https://stats.uptimerobot.com/4wvr6UzvYm/797417947)
-[![api uptime](https://img.shields.io/uptimerobot/ratio/m797417947-fe687be74c0746276cc9cb3d?label=api_uptime)](https://stats.uptimerobot.com/4wvr6UzvYm/797417947)
+[![api status](https://img.shields.io/uptimerobot/status/m797417947-fe687be74c0746276cc9cb3d?label=api%20status)](https://stats.uptimerobot.com/4wvr6UzvYm/797417947)
+[![api uptime](https://img.shields.io/uptimerobot/ratio/m797417947-fe687be74c0746276cc9cb3d?label=api%20uptime)](https://stats.uptimerobot.com/4wvr6UzvYm/797417947)
 [![cd](https://img.shields.io/github/actions/workflow/status/yavorfingarov/SimpleNewTab/cd.yml?branch=master&label=cd)](https://github.com/yavorfingarov/SimpleNewTab/actions/workflows/cd.yml?query=branch/master)
 [![codeql](https://img.shields.io/github/actions/workflow/status/yavorfingarov/SimpleNewTab/codeql.yml?branch=master&label=codeql)](https://github.com/yavorfingarov/SimpleNewTab/actions/workflows/codeql.yml?query=branch/master)
 [![loc](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/yavorfingarov/08cdfad3804ed47e85cddf37d00755a0/raw/lines-of-code.json)](https://github.com/yavorfingarov/SimpleNewTab/actions/workflows/cd.yml?query=branch/master)
--- src/SimpleNewTab.Api/Bing/BingService.cs
+++ src/SimpleNewTab.Api/Bing/BingService.cs
@@ -6,14 +6,14 @@ namespace SimpleNewTab.Api.Bing
     {
         private readonly HttpClient _HttpClient;
         private readonly BingConfiguration _Configuration;
-        private readonly IValidator<ImageArchiveDto> _Validator;
-        private readonly IMapper<ImageArchiveDto, ImageMetadata> _Mapper;
+        private readonly IValidator<ImageDto> _Validator;
+        private readonly IMapper<ImageDto, ImageMetadata> _Mapper;
 
         public BingService(
             HttpClient httpClient,
             BingConfiguration configuration,
-            IValidator<ImageArchiveDto> validator,
-            IMapper<ImageArchiveDto, ImageMetadata> mapper)
+            IValidator<ImageDto> validator,
+            IMapper<ImageDto, ImageMetadata> mapper)
         {
             _HttpClient = httpClient;
             _Configuration = configuration;
@@ -26,13 +26,15 @@ namespace SimpleNewTab.Api.Bing
             var url = $"{_Configuration.BaseUrl}{_Configuration.LatestImageRoute}";
             using var request = new HttpRequestMessage(HttpMethod.Get, url);
             request.Headers.UserAgent.Add(Metadata.ProductInfo);
-            request.Headers.UserAgent.Add(new ProductInfoHeaderValue(_Configuration.UserAgentComment));
+            var comment = new ProductInfoHeaderValue(_Configuration.UserAgentComment);
+            request.Headers.UserAgent.Add(comment);
             using var response = await _HttpClient.SendAsync(request, cancellationToken);
             response.EnsureSuccessStatusCode();
             var imageArchiveDto = await response.Content.ReadFromJsonAsync<ImageArchiveDto>(cancellationToken);
-            ArgumentNullException.ThrowIfNull(imageArchiveDto);
-            await _Validator.ValidateAndThrowAsync(imageArchiveDto, cancellationToken);
-            var imageMetadata = _Mapper.Map(imageArchiveDto);
+            var imageDto = imageArchiveDto?.Images?.Single();
+            ArgumentNullException.ThrowIfNull(imageDto);
+            _Validator.ValidateAndThrow(imageDto);
+            var imageMetadata = _Mapper.Map(imageDto);
 
             return imageMetadata;
         }
